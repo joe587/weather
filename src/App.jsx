5a3c4880622305c417eff17b1f4b811427dd05aa
joe587 <rocky3002@yahoo.com>
2025-04-26 09:41:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from 'react'
+
+
+import './App.css'
+
+function App() {
+  const [city, setCity] = useState('')
+  const [temp, setTemp] = useState(false)
+  const [weather,setWeather]=useState([])
+    useEffect(()=>{
+      const url="https://api.weatherapi.com/v1/forecast.json"
+    const api_key="412b492bf8de4497ae193119252801"
+    fetch(`${url}?key=${api_key}&q=hyderabad,visakhapatnam,vijayawada,anakapalle,tuni,bangalore,chennai,tamilnadu`)
+      .then(response=>response.json())
+      .then(data=>{
+        setWeather(data)
+        setTemp(true)
+      })
+
+},[])
+const searchWeather = ()=>{
+  const url="https://api.weatherapi.com/v1/forecast.json"
+  const api_key="412b492bf8de4497ae193119252801"
+  fetch(`${url}?key=${api_key}&q=${city}`)
+  .then(response=>{
+    if(!response.ok){
+      alert("location not found")
+    }
+    else{
+      response.json()
+      .then(data=>setWeather(data))
+      setCity('')
+    }
+  })
+}
+
+  return (
+    <>
+      <div className='main'>
+        <div className='child1'>
+          <input id='inp' 
+          value={city}
+          type='text'
+          placeholder=' Prasad Enter your city name...'
+          onChange={(event)=>{setCity(event.target.value)}}
+        
+          />
+          <button onClick={searchWeather} id="btn">Search</button>
+
+
+      </div>
+      {temp && <>
+      <div className='child2'>
+        <img src={weather.current.condition.icon}/>
+        <h1 id="tempc">{weather.current.temp_c}℃</h1>
+        <h1 id="place">{weather.location.name}</h1>
+        <h1 id='region'>{weather.location.region}</h1>
+        <h3 id='region'>{weather.location.country}</h3>
+        </div>
+        <div className='child3'>
+          <h1>sunrise:{weather.forecast.forecastday[0].astro.sunrise}</h1>
+          <h1>sunset:{weather.forecast.forecastday[0].astro.sunset}</h1>
+          <h1>max:{weather.forecast.forecastday[0].day.maxtemp_c}℃</h1>
+          <h1>min:{weather.forecast.forecastday[0].day.mintemp_c}℃</h1>
+
+          </div>
+          </>}
+
+      </div>
+      
+    </>
+  )
+}
+
+export default App
